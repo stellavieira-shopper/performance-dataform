f186d7e27c3271411b59f09599283ac2a47254f1
Stella Hadassa Alves Vieira <stella.vieira@shopper.com.br>
2026-07-14 08:55:55 -0300

    feat: initial dataform structure - curated tables + extract

---
+++ includes/constants.js
@@ -0,0 +1,6 @@
+const PROD_PROJECT    = "shopper-datalakehouse-prod";
+const QA_PROJECT      = "shopper-datalakehouse-qa";
+const TARGET_PROJECT  = "shopper-performance-prod";
+const DATASET         = "Ranking_Performance";
+
+module.exports = { PROD_PROJECT, QA_PROJECT, TARGET_PROJECT, DATASET };
